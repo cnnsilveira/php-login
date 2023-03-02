1997e20d2ccc61243c18219126cf6143a72144a9
caionunes <caionunessteam@hotmail.com>
2023-03-02 19:38:01 -0300

    js folder structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// Background effect
+
 $(document).ready(() => {
+
     var movementStrength = 25;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
@@ -9,5 +12,31 @@ $(document).ready(() => {
         var newValueX = width * pageX * -1 - 25;
         var newValueY = height * pageY * -1 - 50;
         $('.bg-container').css("background-position", newValueX + "px " + newValueY + "px");
+
     });
+
+});
+
+
+
+// Header 'Welcome' effect
+
+$(document).ready(setTimeout(() => {
+
+    $('.js-welcome-first').css({'visibility': 'hidden'});
+
+    const secondEl = $('.js-welcome-second');
+    secondEl.animate({left:'10px'}, 350);
+    setTimeout(() => {
+        secondEl.css({'padding': '0'});
+    }, 500);
+
+}, 1000));
+
+
+
+// Form fill
+
+$(document).ready(() => {
+    
 });
